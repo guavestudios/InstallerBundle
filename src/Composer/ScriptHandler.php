ce8b209401cfb38a83bd8d83f748f5f052931886
Christopher Stadler <christopher.stadler@guavestudios.com>
2021-05-11 11:08:45 +0200

    fix: change file lines

--- src/Composer/ScriptHandler.php
+++ src/Composer/ScriptHandler.php
@@ -6,16 +6,16 @@ use Composer\Script\Event;
 
 class ScriptHandler
 {
-    public static function replaceNameDir(Event $event)
+    public static function replaceNameDir(Event $event): void
     {
         $project = $event->getArguments()[0];
-        
+
         $dirs = [
-            'files/project' => 'files/' . $project,
-            'frontend/project' => 'frontend/' . $project,
-            'templates/project' => 'templates/' . $project,
+            'files/template' => 'files/' . $project,
+            'frontend/template' => 'frontend/' . $project,
+            'templates/template' => 'templates/' . $project,
         ];
-        
+
         foreach ($dirs as $old => $new) {
             if (!rename($old, $new)) {
                 echo 'Could not rename ' . $old . ' to ' . $new;
@@ -23,26 +23,25 @@ class ScriptHandler
         }
     }
 
-    public static function replaceNameFile(Event $event)
+    public static function replaceNameFile(Event $event): void
     {
         $project = $event->getArguments()[0];
-        
+
         $files = [
-            '.env.example' => [24, 27, 28],
-            'Gruntfile.js' => [44],
+            '.env.local.guave' => [4],
+            '.env.stage' => [4],
+            '.env.prod' => [4],
+            'Gruntfile.js' => [47],
             'jsconfig.json' => [14],
             'webpack.config.js' => [25],
-            'build/dotenv.json' => [2, 3, 4],
-            'config/custom.yml' => [3],
-            'config/parameters.dev.yml' => [5, 6, 7],
-            'config/parameters.stage.yml' => [5, 6, 7],
-            'config/parameters.live.yml' => [5, 6, 7],
+            'build/dotenv.json' => [2, 3, 4, 6],
+            'config/custom.yml' => [2],
             'contao/config/config.php' => [23],
             'system/config/localconfig.dev.php' => [6],
             'system/config/localconfig.live.php' => [6],
             'templates/' . $project . '/base.html5' => [23, 54],
         ];
-        
+
         foreach ($files as $file => $lines) {
             if (!self::fileNameReplacer($file, $lines, $project)) {
                 echo 'Could not rename ' . $file . ' on lines ' . implode(', ', $lines);
@@ -50,7 +49,7 @@ class ScriptHandler
         }
     }
 
-    public function fileNameReplacer($file, $lines, $newName)
+    public static function fileNameReplacer($file, $lines, $newName): bool
     {
         $loadedFile = file($file);
         $result = $loadedFile;
